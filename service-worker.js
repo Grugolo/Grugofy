fa26f54731902a019a59add0a59a6449a4462f73
Grugolo <fedego02@outlook.com>
2026-09-01 10:04:58 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE = "grugofy-v1";
+
+const FILES = [
+  "/Jukebox/",
+  "/Jukebox",
+  "/Jukebox/style.css",
+  "/Jukebox/js/main.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+
