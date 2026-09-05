5f9a86935853b88362867b4d15301278370c2672
Grugolo <fedego02@outlook.com>
2026-09-05 02:22:23 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 // Sostituisci con le tue chiavi da Google Cloud Console (YouTube Data API v3).
 // Puoi metterne più di una: verranno provate in sequenza se una finisce la quota.
 export const YT_API_KEY = [
-  'yyy',
-  'xxx',
-  'zzz',
+  'AIzaSyC1oGf7bmj7Wx8MSM1Pi01bp9v_JMu7e6w', 
+  'AIzaSyBwZsEchAW5KOytpVE6lqRQaXYdOrsbYT0', 
+  'AIzaSyDRAPLHt6V5eMb4l8fSKobFLy0q47UqUEU'
 ];
